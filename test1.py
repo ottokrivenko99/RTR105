47ab4b613f2e10a20192dc7775c7a0d55f9344a2
user <ottokrivenko87@rtu.lv>
2018-10-17 10:37:33 +0300

    20181017_10_40

---
+++ test1.py
@@ -0,0 +1,119 @@
+##n = 5
+##while n > 0 :
+##    print(n)
+##    n = n - 1
+##    print('Blastoff!')
+##    print(n)
+
+
+##An infinite loop
+##n = 5
+##while n > 0
+##print('Lather')
+##print('Rinse')
+##print('Dry off!')
+
+
+##n = 0
+##while n > 0 :
+##    print('Lather')
+##    print('Rinse')
+##    print('Dry off!')
+
+
+##Breaking out of a loop
+##while True:
+##    line = input('>hello there')
+##    if line == 'done' :
+##        break
+##    print(line)
+##    print('Done!')
+
+
+##Finishing an iteration with continue
+
+##while True:
+##    line = input('>hello there')
+##    if line[0] == '#' :
+##        continue
+##    if line == 'done' :
+##        break
+##    print(line)
+##    print('Done!')
+##    
+
+
+
+
+##A Simple definite loop
+##for i in [5, 4, 3, 2, 1] :
+##    print(i)
+##    print('Blastoff!')
+
+
+
+
+##Looping through a Set
+##print('Before')
+##for thing in [9, 41, 12, 3, 74, 15] :
+##    print(thing)
+##    print('After')
+
+
+
+##Finding the larges value
+##largest_so_far = -1
+##print('Before',largest_so_far)
+##for the_num in [9, 41, 12, 3, 74, 15] :
+##    if the_num > largest_so_far :
+##        largest_so_far = the_num
+##        print(largest_so_far,the_num)
+##
+##    print('After',largest_so_far)
+
+
+##
+##Counting in a loop
+##zork = 0
+##print('Before', zork)
+##for thing in :
+##    zork = zork + 1
+##    print (zork, thing)
+##    print('After', zork)
+
+
+
+
+##Filtering in a loop
+##print('Before')
+##for value in [9, 41, 12, 3, 74, 15]:
+##    if value > 20:
+##        print('Large number',value)
+##print('After')
+
+
+
+##Search using a boolean variable
+##found = False
+##print('Before',found)
+##for value in [9, 41, 12, 3, 74, 15]:
+##    if value == 3:
+##        found = True
+##    print(found, value)
+##print('After',found)
+
+
+
+##Finding the Average in loop
+##count = 0
+##sum = 0
+##print('Before', count, sum)
+##for value in [9, 41, 12, 3, 74, 15]:
+##    count = count + 1
+##    sum = sum + value
+##    print(count,sum,value)
+##print('After',count,sum,sum/count)
+
+
+
+    

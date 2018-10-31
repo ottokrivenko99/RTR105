1eb2885e3aa2690a499208d6457e5d6625cb88c8
user <ottokrivenko87@rtu.lv>
2018-10-31 11:57:02 +0200

    20181031_11_55

---
+++ sin_caur_summu_versia4.py
@@ -0,0 +1,17 @@
+##-*- coding: utf-8 -*-
+from math import sin
+x = float(input("Lietotāj, lūdzu, ievadi argumentu (x): "))
+
+y = sin(x)
+print("sin(%.2f) = %.2f"%(x,y))
+
+
+k = 0
+a = (-1)**0*x**1/(1)
+S = a
+print("a0 = %.2f S0 = %2f"%(a,S))
+
+
+k = 1
+##a = a * (-1)*x*x/(2*3)
+a = a * (-1)*x*x/(2*3)
